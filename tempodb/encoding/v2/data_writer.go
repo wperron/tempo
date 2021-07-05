65c535e0024bcb0fdd12d4a1e3c3d791308f47d7
William Perron <hey@wperron.io>
2021-07-05 16:46:08 -0400

    inline v1 data writer in v2

--- tempodb/encoding/v2/data_writer.go
+++ tempodb/encoding/v2/data_writer.go
@@ -6,54 +6,99 @@ import (
 
 	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding/common"
-	v1 "github.com/grafana/tempo/tempodb/encoding/v1"
 )
 
-type dataWriter struct {
-	v1Writer common.DataWriter
-	v1Buffer *bytes.Buffer
+// meteredWriter is a struct that is used to count the number of bytes
+// written to a block after compression.  Unfortunately the compression io.Reader
+// returns bytes before compression so this is necessary to know the actual number of
+// byte written.
+type meteredWriter struct {
+	wrappedWriter io.Writer
+	bytesWritten  int
+}
 
-	outputWriter io.Writer
+func (m *meteredWriter) Write(p []byte) (n int, err error) {
+	m.bytesWritten += len(p)
+	return m.wrappedWriter.Write(p)
+}
+
+type dataWriter struct {
+	buffer            *bytes.Buffer
+	outputWriter      *meteredWriter
+	compressionWriter io.WriteCloser
+
+	pool     WriterPool
+	objectRW common.ObjectReaderWriter
 }
 
 // NewDataWriter creates a paged page writer
 func NewDataWriter(writer io.Writer, encoding backend.Encoding) (common.DataWriter, error) {
-	v1Buffer := &bytes.Buffer{}
-	v1Writer, err := v1.NewDataWriter(v1Buffer, encoding)
+	pool, err := GetWriterPool(encoding)
+	if err != nil {
+		return nil, err
+	}
+
+	outputWriter := &meteredWriter{
+		wrappedWriter: writer,
+	}
+
+	compressionWriter, err := pool.GetWriter(outputWriter)
 	if err != nil {
 		return nil, err
 	}
 
 	return &dataWriter{
-		v1Writer:     v1Writer,
-		v1Buffer:     v1Buffer,
-		outputWriter: writer,
+		buffer:            &bytes.Buffer{},
+		outputWriter:      outputWriter,
+		pool:              pool,
+		compressionWriter: compressionWriter,
+		objectRW:          NewObjectReaderWriter(),
 	}, nil
 }
 
 // Write implements DataWriter
 func (p *dataWriter) Write(id common.ID, obj []byte) (int, error) {
-	return p.v1Writer.Write(id, obj)
+	return p.objectRW.MarshalObjectToWriter(id, obj, p.buffer)
 }
 
 // CutPage implements DataWriter
 func (p *dataWriter) CutPage() (int, error) {
-	_, err := p.v1Writer.CutPage()
+	var err error
+	p.compressionWriter, err = p.pool.ResetWriter(p.outputWriter, p.compressionWriter)
 	if err != nil {
 		return 0, err
 	}
 
-	// v1Buffer currently has all of the v1 bytes. let's wrap it in our page and write
-	bytesWritten, err := marshalPageToWriter(p.v1Buffer.Bytes(), p.outputWriter, constDataHeader)
+	buffer := p.buffer.Bytes()
+	_, err = p.compressionWriter.Write(buffer)
 	if err != nil {
 		return 0, err
 	}
-	p.v1Buffer.Reset()
+
+	// now clear our v0 buffer so we can start the new block page
+	p.compressionWriter.Close()
+	p.buffer.Reset()
+
+	// TODO(wperron) lint error that bytesWritten is never used here
+	// bytesWritten := p.outputWriter.bytesWritten
+	p.outputWriter.bytesWritten = 0
+
+	// v1Buffer currently has all of the v1 bytes. let's wrap it in our page and write
+	bytesWritten, err := marshalPageToWriter(p.buffer.Bytes(), p.outputWriter, constDataHeader)
+	if err != nil {
+		return 0, err
+	}
+	p.buffer.Reset()
 
 	return bytesWritten, err
 }
 
 // Complete implements DataWriter
 func (p *dataWriter) Complete() error {
-	return p.v1Writer.Complete()
+	if p.compressionWriter != nil {
+		p.pool.PutWriter(p.compressionWriter)
+		p.compressionWriter = nil
+	}
+
+	return nil
 }
